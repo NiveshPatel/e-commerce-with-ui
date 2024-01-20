eec146421b10c29b9dd5ec9630036afaa9a16a64
Nivesh Patel <0786nivesh0786@gmail.com>
2024-01-20 20:41:10 +0530

    new

---
+++ src/store/productSlice.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+export const STATUS = Object.freeze({
+    IDLE:'idle',
+    LOADING:'loading',
+    ERROR:'error'
+})
+const productSlice = createSlice({
+    name:"products",
+    initialState:{
+        data:[],
+        status:STATUS.IDLE
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchproducts.pending,(state,action)=>{
+            state.status = STATUS.LOADING
+        })
+        .addCase(fetchproducts.fulfilled,(state,action)=>{
+            state.data = action.payload
+            state.status = STATUS.IDLE
+        })
+        .addCase(fetchproducts.rejected,(state,sction)=>{
+            state.status= STATUS.ERROR
+        })
+    }
+})
+export default productSlice.reducer
+// thunk
+export const fetchproducts = createAsyncThunk("product/fetch",async()=>{
+    const res = await fetch("https://fakestoreapi.com/products")
+    const data = await res.json()
+    return data
+    console.log(data)
+})
